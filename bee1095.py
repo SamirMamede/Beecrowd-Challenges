ae98c43e6d0d8567086e59190e54a63076d7831d
SamirMamede <anuarsamir@gmail.com>
2022-08-19 21:39:20 -0400

    bee1095 Sequencia IJ 1

---
+++ bee1095.py
@@ -0,0 +1,25 @@
+'''
+Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo
+
+Exemplo de Saída
+I=1 J=60
+I=4 J=55
+I=7 J=50
+...
+I=? J=0
+
+'''
+
+I = 1
+J = 60
+
+while J >= 0:
+    print(f"I={I} J={J}")
+    I+=3
+    J-=5
